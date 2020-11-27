18ba8de505a67c29e92bedc376016d1e5ff8f506
Vesa Ranta-aho <vesa.ranta-aho@aalto.fi>
2020-11-27 08:22:01 +0200

    Changed categorical vars to binary vars, added diagnostics

--- testiajo.R
+++ testiajo.R
@@ -1,44 +1,95 @@
 library(rstan)
+library(loo)
 library(tidyverse)
 
 data <- read.csv('heart.csv', sep=',')
-s <- sample(nrow(data))
-shuffled <- data[s,]
 
-data_train <- head(shuffled, 270)
-data_test <- tail(shuffled, 33)
-x_train <- select(data_train,'age':'thal')
-x_test <- select(data_test,'age':'thal')
+N <- nrow(data)
+
+good_data <- data.frame(matrix(0L, ncol = 28, nrow = N))
+columns <- c("age", "sex", "cp0", "cp1", "cp2", "cp3", "trestbps", "chol", "fbs",
+            "restecg0", "restecg1", "restecg2", "thalach", "exang", "oldpeak", "slope0", "slope1", "slope2",
+            "ca0", "ca1", "ca2", "ca3", "ca4", "thal0", "thal1", "thal2", "thal3", "target")
+
+colnames(good_data) <- columns
+good_data$age <- data$ï..age
+good_data$sex <- data$sex
+good_data$trestbps <- data$trestbps
+good_data$chol <- data$chol
+good_data$fbs <- data$fbs
+good_data$thalach <- data$thalach
+good_data$exang <- data$exang
+good_data$oldpeak <- data$oldpeak
+good_data$target <- data$target
+
+for (i in 1:N) {
+  if (data$cp[i] == 0) good_data$cp0[i] <- 1
+  else if (data$cp[i] == 1) good_data$cp1[i] <- 1
+  else if (data$cp[i] == 2) good_data$cp2[i] <- 1
+  else good_data$cp3[i] <- 1
+  
+  if (data$restecg[i] == 0) good_data$restecg0[i] <- 1
+  else if (data$restecg[i] == 1) good_data$restecg1[i] <- 1
+  else good_data$restecg2[i] <- 1
+  
+  if (data$slope[i] == 0) good_data$slope0[i] <- 1
+  else if (data$slope[i] == 1) good_data$slope1[i] <- 1
+  else good_data$slope2[i] <- 1
+  
+  if (data$ca[i] == 0) good_data$ca0[i] <- 1
+  else if (data$ca[i] == 1) good_data$ca1[i] <- 1
+  else if (data$ca[i] == 2) good_data$ca2[i] <- 1
+  else if (data$ca[i] == 3) good_data$ca3[i] <- 1
+  else good_data$ca4[i] <- 1
+  
+  if (data$thal[i] == 0) good_data$thal0[i] <- 1
+  else if (data$thal[i] == 1) good_data$thal1[i] <- 1
+  else if (data$thal[i] == 2) good_data$thal2[i] <- 1
+  else good_data$thal3[i] <- 1
+}
+
+
+# PREDICTING
+
+s <- sample(nrow(good_data))
+shuffled <- good_data[s,]
+
+N_test <- 33
+
+data_train <- head(shuffled, N - N_test)
+data_test <- tail(shuffled, N_test)
+x_train <- select(data_train,'age':'thal3')
+x_test <- select(data_test,'age':'thal3')
 
 standata <- list(y=data_train$target, N=nrow(data_train), M=ncol(data_train)-1, P=nrow(data_test), x=x_train, pred_x=x_test)
 
-a <- stan(file = 'testmodel.stan', data = standata)
-b <- monitor(a)
-c <- rstan::extract(a)
-plot(c$y_pred)
-hist(c$y_pred)
-hist(data$target)
-d <- list()
-drwas <- as.data.frame(a)
-calc <- 0
-drwas
-abc <- select(drwas,'y_pred[1]':'y_pred[33]')
-abc <- as.matrix(abc,ncols=33)
-ii <- 1
-for(i in 1:33) {
-  
-  d[ii] <- mean(abc[,i])
-  ii= ii+1
+model <- stan(file = 'testmodel.stan', data = standata)
+mon <- monitor(model)
+
+predictions <- data.frame(matrix(0L, ncol = 3, nrow = N_test))
+cols <- c("mu", "prediction", "target")
+colnames(predictions) <- cols
+predictions$target <- data_test$target
+
+draws <- as.data.frame(model)
+preds <- as.matrix(select(draws,'y_pred[1]':'y_pred[33]'), ncols=N_test)
+
+wrong <- 0
+for (i in 1:N_test) {
+  mu <- mean(preds[,i])
+  predictions$mu[i] <- mu
+  if (mu >= 0.5) predictions$prediction[i] <- 1
+  if (predictions$prediction[i] != predictions$target[i]) wrong <- wrong + 1
 }
+wrong
 
-totta = list()
-uusi <- data_test$target
+# LOO
 
-for(k in 1:33) {
-  if(  (d[k]> 0.5 && uusi[k]==1) || ( d[k] < 0.5 && uusi[k] ==0) ) {
-    totta[k] <- TRUE
-  }
-  
-}
+model_loo <- loo(model)
+model_loo$elpd_loo
 
+barplot(model_loo$diagnostics$pareto_k, ylim=c(-0.2, 0.8))
+abline(a=0.7, b=0, col='red')
+title('k values')
 
+model_loo$p_loo
--- testmodel.stan
+++ testmodel.stan
@@ -1,31 +1,27 @@
 // The input data is a vector 'y' of length 'N'.
 data {
-  int<lower=0> N;
-  int<lower=0> M;
-  int<lower=0> P;
-  int<lower=0,upper=1> y[N]; // target
-  row_vector[M] x[N]; // variables
-  row_vector[M] pred_x[P];
+  int<lower=0> N; // Number of observations
+  int<lower=0> M; // Number of features
+  int<lower=0> P; // Number of predictions
+  int<lower=0,upper=1> y[N]; // Target
+  row_vector[M] x[N]; // Variables
+  row_vector[M] pred_x[P]; // Prediction variables
 }
 
-// The parameters accepted by the model. Our model
-// accepts two parameters 'mu' and 'sigma'.
 parameters {
   real alpha;
-  vector[M] beta; //slope
-  real mu[M];
-  real<lower=0> sigma[M];
+  vector[M] beta;
+  // real mu[M];
+  // real<lower=0> sigma[M];
 }
 
-// The model to be estimated. We model the output
-// 'y' to be normally distributed with mean 'mu'
-// and standard deviation 'sigma'.
 model {
 
   alpha ~ normal(0, 100);
   for (m in 1:M) {
-    mu[m] ~ normal(0, 100);
-    sigma[m] ~ inv_chi_square(0.1);
+    // mu[m] ~ normal(0, 100);
+    // sigma[m] ~ inv_chi_square(0.1);
+    beta[m] ~ normal(0, 100);
   }
   
   for (n in 1:N) {
@@ -36,8 +32,11 @@ model {
 
 generated quantities {
   int<lower=0,upper=1> y_pred[P];
+  vector[N] log_lik;
   for (p in 1:P) {
     y_pred[p] = bernoulli_rng(inv_logit(alpha + pred_x[p]*beta));
   }
+  for (n in 1:N)
+    log_lik[n] = bernoulli_logit_lpmf(y[n] | alpha + x[n] * beta);
 }
 
